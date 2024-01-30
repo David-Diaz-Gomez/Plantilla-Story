f83efb1f4851081f4c2734737d6d70c403763f4a
David-Diaz-Gomez <davidgerdiaz777@gmail.com>
2024-01-30 13:36:49 -0500

    pequeños cambios

--- Plantilla/app2.js
+++ Plantilla/app2.js
@@ -13,13 +13,6 @@ require([
     // Reemplaza con tu API Key obtenida en tu cuenta developer
   esriConfig.apiKey = "AAPK70593acd587d4c8c90ea672129db5386KUUOo0zmbNlIIa8LXOQR1bB4x9fbhyAZxlj49pvQTbwlH8xMRDtduOCn0QYOlAex"; // Reemplaza con tu API Key
 
-  // Cargar el primer WebMap como la capa "antes"
-  var webmapBefore = new WebMap({
-    portalItem: {
-      id: "0d1e1ecbc57b4efba6165119f2696998" // ID del primer WebMap (cambiar id de webmap)
-    }
-  });
-
   // Cargar el segundo WebMap como la capa "después"
   var webmapAfter = new WebMap({
     portalItem: {
@@ -37,14 +30,12 @@ require([
 
   // Esperar a que se cargue el segundo WebMap
   view.when(function() {
-    // Añadir las capas del primer WebMap al segundo WebMap
-    webmapAfter.addMany(webmapBefore.layers.items);
 
     // Añadir la funcionalidad Swipe al mapa
     var swipe = new Swipe({
       view: view,
-      leadingLayers: [webmapAfter.layers.items[0]], // Capa principal del segundo WebMap
-      trailingLayers: [webmapAfter.layers.items[1]], // Capa secundaria del segundo WebMap
+      leadingLayers: [webmapAfter.layers.items[1]], // Capa principal del segundo WebMap
+      trailingLayers: [webmapAfter.layers.items[0]], // Capa secundaria del segundo WebMap
       position: 50,
       direction: "horizontal"
     });
