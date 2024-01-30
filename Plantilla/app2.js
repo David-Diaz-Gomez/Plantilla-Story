df3eb2bb2e67ffe79a058007fd8c37cf8e14ebf4
David-Diaz-Gomez <davidgerdiaz777@gmail.com>
2024-01-30 15:14:04 -0500

    comentarioss

--- Plantilla/app2.js
+++ Plantilla/app2.js
@@ -13,29 +13,29 @@ require([
     // Reemplaza con tu API Key obtenida en tu cuenta developer
   esriConfig.apiKey = "AAPK70593acd587d4c8c90ea672129db5386KUUOo0zmbNlIIa8LXOQR1bB4x9fbhyAZxlj49pvQTbwlH8xMRDtduOCn0QYOlAex"; // Reemplaza con tu API Key
 
-  // Cargar el segundo WebMap como la capa "después"
+  // Cargar el WebMap"
   var webmapAfter = new WebMap({
     portalItem: {
       id: "24da1e8a67534de58d5136a7f12d5443" // ID del WebMap (cambiar id de webmap)
     }
   });
 
-  // Crear la vista del mapa con el segundo WebMap
+  // Crear la vista del mapa con el WebMap
   var view = new MapView({
     container: "map1", // ID del contenedor HTML
     map: webmapAfter,
-    center: [-74.0817, 4.6097], // Coordenadas de Bogotá
+    center: [-74.0817, 4.6097], // Coordenadas de Bogotá (se centraliza en Bogotá)
     zoom: 12 // Nivel de zoom deseado
   });
 
-  // Esperar a que se cargue el segundo WebMap
+  // Esperar a que se cargue el WebMap
   view.when(function() {
 
     // Añadir la funcionalidad Swipe al mapa
     var swipe = new Swipe({
       view: view,
-      leadingLayers: [webmapAfter.layers.items[1]], // Capa principal del segundo WebMap
-      trailingLayers: [webmapAfter.layers.items[0]], // Capa secundaria del segundo WebMap
+      leadingLayers: [webmapAfter.layers.items[1]], // Capa principal del  WebMap
+      trailingLayers: [webmapAfter.layers.items[0]], // Capa secundaria del WebMap
       position: 50,
       direction: "horizontal"
     });
